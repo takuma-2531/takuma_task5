ba0788e797c5050e15cee3ca08aaa0030664be20
Takuma Ogawa <nyabo10_31@yahoo.co.jp>
2021-06-27 18:20:20 +0900

    Initial Commit

---
+++ takuma_task5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  takuma_task5
+//
+//  Created by 小川卓馬 on 2021/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
